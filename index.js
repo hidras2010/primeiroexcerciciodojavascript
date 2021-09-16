7a4b4318a0827d8d34669aa2a8d34ccebf34cfbf
hidras2010 <hidras2010ht@gmail.com>
2021-09-16 15:48:59 -0300

    Execicio da JS

---
+++ index.js
@@ -0,0 +1,38 @@
+// let caneta ="cardozo muamba"
+// console.log(caneta)
+// let mariamba ="endan+erica"
+// console.log(mariamba)
+// if (endan=cardozo) {
+    
+// } else {
+    
+// }
+
+
+let entrada = prompt(`Escolhe um: "Alison","Marcelo" , "Emanuel" , "Lucas" , "Bento"`)
+// console.log(cores)
+
+var nomes = ["Alison","Marcelo" , "Emanuel" , "Lucas" , "Bento"]
+// console.log(nomes.sort())
+
+if(nomes[0]==entrada){
+    alert("valor encontrado: "+nomes[0])
+
+}else if(nomes[1]==entrada){
+    alert("valor encontrado: "+nomes[1])
+}else if(nomes[2]==entrada){
+    alert("valor encontrado: "+nomes[2])
+}else if(nomes[3]==entrada){
+    alert("valor encontrado: "+nomes[3])
+}else if(nomes[4]==entrada){
+    alert("valor encontrado: "+nomes[4])
+}else {
+    alert("valor nao encontrada.porfavor escolhe opcoes acima")
+}
+
+
+   /// console.log(nomes.sort())
+
+   
+
+ 
